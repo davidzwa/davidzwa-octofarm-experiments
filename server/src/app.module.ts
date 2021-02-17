d09f5f9f512e61d697e2375ddf792a10dc08dfdb
davidzwa <davidzwa@gmail.com>
2021-02-17 12:15:41 +0100

    Work on typeorm (#2)
    * Start following prescribed typeorm setup
    
    * Add user entity with username uniqueness, cleanup logs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './api/app.controller';
-import { AppService } from './application/app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app/app.controller";
+import { AppService } from "./app/app.service";
+import { ConfigModule } from "@nestjs/config";
+import { UserModule } from "./users/user.module";
+import { Module } from "@nestjs/common";
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: [".env", ".env.development", ".env.production"]
+        }),
+        // https://docs.nestjs.com/techniques/configuration#using-the-configservice
+        // https://github.com/typeorm/typeorm/blob/master/docs/using-ormconfig.md#which-configuration-file-is-used-by-typeorm
+        TypeOrmModule.forRoot(),
+        UserModule
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,35 @@
 import { NestFactory } from "@nestjs/core";
+import { AddSwagger } from "./extensions/host.extensions";
 import { AppModule } from "./app.module";
-import { AddSwagger } from "./host/extensions/host.extensions";
+import { INestApplication, Logger } from "@nestjs/common";
+import { D, Y } from "./utils/logging.util";
+
+function printPreBootMessage(logger: Logger) {
+    logger.log(`OctoFarm2 ${Y}v${process.env.npm_package_version}${D}`);
+    logger.log(`NodeJS ${Y}${process.version}${D}`);
+    const startDate = new Date().toDateString();
+    const startTime = new Date().toLocaleTimeString();
+    const startMoment = `at ${Y}${startTime}${D} on ${Y}${startDate}${D}`;
+    logger.log(`Booted ${startMoment}!`, );
+
+}
+
+async function printPostBootMessage(logger: Logger, app: INestApplication) {
+    logger.warn(`Server is listening on ${await app.getUrl()}`);
+    logger.warn(`${Y}%s${D}`, "Happy printing!");
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  AddSwagger(app);
-  await app.listen(3000);
+    const logger = new Logger("Main");
+    printPreBootMessage(logger);
+
+    const app = await NestFactory.create(AppModule, {
+        logger: ["error", "warn"]
+    });
+    AddSwagger(app);
+    await app.listen(3000, "127.0.0.1", () => {
+        printPostBootMessage(logger, app);
+    });
 }
 
 bootstrap();
